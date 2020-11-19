ffbeabe1278bc8068343e2c12c21d0e1113d066c
1287361614@qq.com <1287361614@qq.com>
2020-11-19 11:21:15 +0800

    git

---
+++ js/git.js
@@ -0,0 +1,14 @@
+//将文件添加到暂存区
+// git add .
+
+// git remote 命令列出所有远程主机
+// git remote -v 参看远程主机
+// 所使用的远程主机自动被Git命名为origin。如果想用其他的主机名，需要用git clone命令的-o选项指定。
+// git clone -o jQuery https://github.com/jquery/jquery.git
+// git remote
+// jQuery
+
+// git remote show <主机名> 命令加上主机名，可以查看该主机的详细信息。
+// git remote add <主机名> <网址> 命令用于添加远程主机
+// git remote rm <主机名> 命令用于删除远程主机。
+// git remote rename <原主机名> <新主机名>命令用于远程主机的改名。
