527989d6bca4d6f15c7e5161f2cda4aa64f60e60
1287361614@qq.com <1287361614@qq.com>
2020-11-20 10:11:32 +0800

    changelocal

--- js/local.js
+++ js/local.js
@@ -45,3 +45,75 @@
 // 使用一次新的commit,代替上一次提交
 // 如果代码没有任何新的变化,则用来改写上一次commit的提交信息
 // git commit --amend -m [message]
+// 重做一次commit ,并包含指定文件的新变化
+// git commit --amend [file1] [file2]
+
+
+// 分支
+// 列出所有本地分支
+// git branch
+// 列出所有远程分支
+// git branch -r
+// 列出所有本地分支和远程分支
+// git branch -a
+// 新建一个分支，依旧停留在当前分支
+// git branch [branch-name]
+// 新建一个分支，并切换到该分支
+// git checkout -b [branch]
+// 新建一个分支，指向指定commit
+// git branch [branch] [commit]
+// 新建一个分支，与指定的远程分支建立追钟关系
+// git branch --track [branch] [remote-branch]
+// 切换到指定分支，并更新工作区
+// git checkout [branch-name]
+// 切换到上一个分支
+// git checkout -
+// 建立追踪关系，在现有分支与指定的远程分支之间
+// git branch --set-upstream [branch] [remote-branch]
+// 合并指定分支到当前分支
+// git merge [branch]
+// 选择一个commit，合并进当前分支
+// git cherry-pick [commit]
+// 删除分支
+// git branch -d [branch-name]
+// 删除远程分支
+// git push origin --delete [branch-name]
+// git branch -dr [remote/branch]
+
+// 列出所有的tag
+// git tag
+// 新建一个tag 在当前commit
+// git tag [tag]
+// # 新建一个tag在指定commit
+// $ git tag [tag] [commit]
+// # 删除本地tag
+// $ git tag -d [tag]
+// # 删除远程tag
+// $ git push origin :refs/tags/[tagName]
+// # 查看tag信息
+// $ git show [tag]
+// # 提交指定tag
+// $ git push [remote] [tag]
+// # 提交所有tag
+// $ git push [remote] --tags
+// # 新建一个分支，指向某个tag
+// $ git checkout -b [branch] [tag]
+
+// 显示有变更的文件
+// git status
+// 显示当前分支的版本历史
+// git log
+// 显示commit 历史，以及每次commit 发生更改变更的文件 具有详细的文件辛信息
+// git log --stat
+// 搜索提交历史，根据关键词
+// git log -S [keyword]
+// 显示某个commit 之后的所有变动 每个commit 占据一行
+// git log [tag] HEAD --pretty=format:%s
+// 显示某个commit之后的所有变动，其"提交说明"必须符合搜索条件
+// git log [tag] HEAD --grep feature
+
+// 显示某个文件的版本历史，包括文件改名
+// git log --follow [file]
+// git what changed [file]
+// 显示指定文件相关的每一次diff
+// 
