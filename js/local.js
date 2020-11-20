c4844547290c2a3e21518ebb39fb2578f4508982
1287361614@qq.com <1287361614@qq.com>
2020-11-20 11:41:58 +0800

    all

--- js/local.js
+++ js/local.js
@@ -116,4 +116,73 @@
 // git log --follow [file]
 // git what changed [file]
 // 显示指定文件相关的每一次diff
-// 
+// git log -p [file]
+// 显示过去5次提交
+// git log -5 --pretty --oneline
+// 显示过去所有提交过的用户，按提交次数排序
+// git shortlog -sh
+// 显示指定文件是什么人在什么时间修改过
+// git blame [file]
+// 显示暂存区和工作区的差异
+// git diff
+// 显示暂存区和上一个commit 的差异
+// git diff --cached [file]
+// 显示工作区与当前分支最新commit 之间的差异
+// git diff HEAD
+// 显示两次提交之间的差距
+// git diff [first-branch] ... [second-branch]
+// 显示今天你写了多少行代码
+// git diff --shortstat "@{0 day ago}"
+// 显示某次提交的元数据和内容变化
+// git show [commit]
+// 显示某次提交发生变化的文件
+// git show --name-only [commit]
+// 显示某次提交时，某个文件的内容
+// git show [commit]:[filename]
+// 显示当前分支的最近几次提交
+// git reflog
+
+# 下载远程仓库的所有变动
+$ git fetch [remote]
+# 显示所有远程仓库
+$ git remote -v
+# 显示某个远程仓库的信息
+$ git remote show [remote]
+# 增加一个新的远程仓库，并命名
+$ git remote add [shortname] [url]
+# 取回远程仓库的变化，并与本地分支合并
+$ git pull [remote] [branch]
+# 上传本地指定分支到远程仓库
+$ git push [remote] [branch]
+# 强行推送当前分支到远程仓库，即使有冲突
+$ git push [remote] --force
+# 推送所有分支到远程仓库
+$ git push [remote] --all
+
+
+
+# 恢复暂存区的指定文件到工作区
+$ git checkout [file]
+# 恢复某个commit的指定文件到暂存区和工作区
+$ git checkout [commit] [file]
+# 恢复暂存区的所有文件到工作区
+$ git checkout .
+# 重置暂存区的指定文件，与上一次commit保持一致，但工作区不变
+$ git reset [file]
+# 重置暂存区与工作区，与上一次commit保持一致
+$ git reset --hard
+# 重置当前分支的指针为指定commit，同时重置暂存区，但工作区不变
+$ git reset [commit]
+# 重置当前分支的HEAD为指定commit，同时重置暂存区和工作区，与指定commit一致
+$ git reset --hard [commit]
+# 重置当前HEAD为指定commit，但保持暂存区和工作区不变
+$ git reset --keep [commit]
+# 新建一个commit，用来撤销指定commit
+# 后者的所有变化都将被前者抵消，并且应用到当前分支
+$ git revert [commit]
+# 暂时将未提交的变化移除，稍后再移入
+$ git stash
+$ git stash po
+
+# 生成一个可供发布的压缩包
+$ git archive
